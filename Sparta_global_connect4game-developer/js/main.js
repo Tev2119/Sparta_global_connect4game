b1c7838e057c07d0c8cc29a15f364b930aca60d4
tech-w93a <tmuparadzi@spartaglobal.com>
2019-02-06 11:59:08 +0000

    have coin on click function to return array bottom up

--- Sparta_global_connect4game-developer/js/main.js
+++ Sparta_global_connect4game-developer/js/main.js
@@ -2,30 +2,80 @@ $(document).ready(function(){
   console.dir(document);
   console.log("hello");
 
-
-var gameBoard = document.getElementsByClassName('container');
-for(var i=0; i<gameBoard.length; i++){
-  // var see= gameBoard[i].addEventListner('click');
-  console.log(gameBoard[i]);
-}
-
 var gridval = $(".col-md-1").each(function(index){
-  console.log(index+":"+$(this).text());
+/*  console.log(index+":"+$(this).text());*/
+});
+// console.log(gridval)
+
+var coinDrop1 = document.getElementsByName('coin1');
+$(".coin1").click(function(){
+  for (var i=64 ; i>7; i-=8){
+console.log(gridval[i]);
+
+  }
+
+});
+var coinDrop2 = document.getElementsByName('coin2');
+$(".coin2").click(function(){
+  for (var i=65 ; i>8; i-=8){
+console.log(gridval[i]);
+
+  }
+
+});
+var coinDrop3 = document.getElementsByName('coin3');
+$(".coin3").click(function(){
+  for (var i=66 ; i>9; i-=8){
+console.log(gridval[i]);
+
+  }
+
+});
+var coinDrop4 = document.getElementsByName('coin4');
+$(".coin4").click(function(){
+  for (var i=67 ; i>10; i-=8){
+console.log(gridval[i]);
+
+  }
+
+});
+var coinDrop5 = document.getElementsByName('coin5');
+$(".coin5").click(function(){
+  for (var i=68 ; i>11; i-=8){
+console.log(gridval[i]);
+
+  }
+
+});
+var coinDrop6 = document.getElementsByName('coin6');
+$(".coin6").click(function(){
+  for (var i=69 ; i>12; i-=8){
+console.log(gridval[i]);
+
+  }
+
+});
+var coinDrop7 = document.getElementsByName('coin7');
+$(".coin7").click(function(){
+  for (var i=70 ; i>13; i-=8){
+console.log(gridval[i]);
+
+  }
+
+});
+var coinDrop8 = document.getElementsByName('coin8');
+$(".coin8").click(function(){
+  for (var i=71 ; i>14; i-=8){
+console.log(gridval[i]);
+
+  }
+
 });
 
-console.log(gridval);
 
-function coindrop(c=64; c>gridval.length c-8){
-  console.log(gradval[c]);
-}
-
-
-
-
-
-
-
-
+//  for (var i=8 ; i<=gridval.length; i-=8){
+//   console.log(gridval[i]);
+// }
 
 
 
