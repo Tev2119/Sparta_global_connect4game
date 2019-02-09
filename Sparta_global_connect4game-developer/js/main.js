7986cb21e6768314293f4391b0c4ea1fd256c0aa
tech-w93a <tmuparadzi@spartaglobal.com>
2019-02-09 20:25:53 +0000

    finalized game win arrays and winner plus instructions

--- Sparta_global_connect4game-developer/js/main.js
+++ Sparta_global_connect4game-developer/js/main.js
@@ -1,91 +1,116 @@
 $(document).ready(function(){
-  console.dir(document);
-  console.log("hello");
+  // console.dir(document);
+  // console.log("hello");
 
-  // var coinDrop1 = document.getElementsByName('coin1');
-  //
-  // var coinDrop2 = document.getElementsByName('coin2');
-  //
-  // var coinDrop3 = document.getElementsByName('coin3');
-  //
-  // var coinDrop4 = document.getElementsByName('coin4');
-  //
-  // var coinDrop5 = document.getElementsByName('coin5');
-  //
-  // var coinDrop6 = document.getElementsByName('coin6');
-  //
-  // var coinDrop7 = document.getElementsByName('coin7');
-  //
-  // var coinDrop8 = document.getElementsByName('coin8');
+alert('HOW TO PLAY Fusion 4 plays alot like connect4 however the winning sequences have been extended making the dificulty harder. Click the coinDrop buttons to drop your coin (player1 = 1 & player2 =2) and match a concequetive array of either coin 4 times to win')
 
   var player_color=[];
   // player_1.style.color="red"
   var player_1 = 1;
   // $(player_1)
-  player_2 = 2;
+  var player_2 = 2;
   // var active_player=0;
 
-  var colum_1 = [64,56,48,40,32,24,16,8]
-  var colum_2 = [65,57,49,41,33,25,17,9]
-  var colum_3 = [66,58,50,42,34,26,18,10]
-  var colum_4 = [67,59,51,43,35,27,19,11]
-  var colum_5 = [68,60,52,44,36,28,20,12]
-  var colum_6 = [69,61,53,45,37,29,21,13]
-  var colum_7 = [70,62,54,46,38,30,22,14]
-  var colum_8 = [71,63,55,47,39,31,23,15]
-  console.log(colum_1[0]);
-  var gridval = $(".col-md-1").each(function(index){
-    /*  console.log(index+":"+$(this).text());*/
+  var colum_1 = [64,56,48,40,32,24,16,8];
+  var colum_2 = [65,57,49,41,33,25,17,9];
+  var colum_3 = [66,58,50,42,34,26,18,10];
+  var colum_4 = [67,59,51,43,35,27,19,11];
+  var colum_5 = [68,60,52,44,36,28,20,12];
+  var colum_6 = [69,61,53,45,37,29,21,13];
+  var colum_7 = [70,62,54,46,38,30,22,14];
+  var colum_8 = [71,63,55,47,39,31,23,15];
+  // console.log(colum_1[0]);
+  var row_1 = [8,9,10,11,12,13,14,15];
+  var row_2 = [16,17,18,19,20,21,22,23];
+  var row_3 = [24,25,26,27,28,29,30,31];
+  var row_4 = [32,33,34,35,36,37,38,39];
+  var row_5 = [40,41,42,43,44,45,46,47];
+  var row_6 = [48,49,50,51,52,53,54,55];
+  var row_7 = [56,57,58,59,60,61,62,63];
+  var row_8 = [64,65,66,67,68,69,70,71];
+  var gridval = $(".col-md-1").each(function(index){//return array of bootsrap grid
+    // console.log(index+":"+$(this).text());
   });
   // console.log(gridval)
 
   var getColumnArray = function(col) {
     switch(col) {
       case 1:
-        return colum_1;
-        break;
+      return colum_1;
+      break;
       case 2:
-        return colum_2;
-        break;
+      return colum_2;
+      break;
       case 3:
-        return colum_3;
-        break;
+      return colum_3;
+      break;
       case 4:
-        return colum_4;
-        break;
+      return colum_4;
+      break;
       case 5:
-        return colum_5;
-        break;
+      return colum_5;
+      break;
       case 6:
-        return colum_6;
-        break;
+      return colum_6;
+      break;
       case 7:
-        return colum_7;
-        break;
+      return colum_7;
+      break;
       case 8:
-        return colum_8;
-        break;
+      return colum_8;
+      break;
     }
   };
-
+  var getRowArray = function(row) {
+    switch(row) {
+      case 0:
+      return row_1;
+      break;
+      case 1:
+      return row_2;
+      break;
+      case 2:
+      return row_3;
+      break;
+      case 3:
+      return row_4;
+      break;
+      case 4:
+      return row_5;
+      break;
+      case 5:
+      return row_6;
+      break;
+      case 6:
+      return row_7;
+      break;
+      case 7:
+      return row_8;
+      break;
+    }
+  };
+  //   var validStartingRowsWin = function() { //valid starting row postions for a left to right win
+  //     return [8,16,24,32,40,48,56,64];
+  // };
   var validStartingSquaresDRWin = function(){ //valid squares to start on for a diagonal right win.
     return [32,33,34,35,36,40,41,42,43,44,48,49,50,51,52,56,57,58,59,60,64,65,66,67,68];
   };
-  var validStartingSquaresDlWin = function(){ //valid squaress to start on the far left for diagnol left win
-    return
-     [35, 36, 37, 38, 39, 43, 44, 45, 46, 47, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 67, 68, 69, 70, 71];
-  }
+  var validStartingSquaresDlWin = function(){ //valid squaress to start on the far right for diagnol left win
+    return [35,36,37,38,39,43,44,45,46,47,51,52,53,54,55,59,60,61,62,63,67,68,69,70,71];
+  };
 
   $(".coin1").click(function(){
     for (var i = colum_1[0]; i >=colum_1.length; i-=8) { //go through anwser array
       // valueOf(gridval[8]);
 
-      if (gridval[i].innerText == ""){
-        // debugger;
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
+        //checking the innertext of the grid
+        // console.log(gridval[i].innerText);
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
           gridval[i].innerText= player_1;
+          player_1.style.color = "red";
           player_1 = 0;
           break;
         }else{
@@ -101,7 +126,7 @@ $(document).ready(function(){
   $(".coin2").click(function(){
     for (var i=colum_2[0] ; i>colum_2.length; i-=8){
 
-      if (gridval[i].innerText == ""){
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
@@ -121,7 +146,7 @@ $(document).ready(function(){
   $(".coin3").click(function(){
     for (var i=colum_3[0] ; i>colum_3.length; i-=8){
 
-      if (gridval[i].innerText == ""){
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
@@ -141,7 +166,7 @@ $(document).ready(function(){
   $(".coin4").click(function(){
     for (var i=colum_4[0] ; i>colum_4.length; i-=8){
 
-      if (gridval[i].innerText == ""){
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
@@ -161,7 +186,7 @@ $(document).ready(function(){
   $(".coin5").click(function(){
     for (var i=colum_5[0] ; i>colum_5.length; i-=8){
 
-      if (gridval[i].innerText == ""){
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
@@ -181,7 +206,7 @@ $(document).ready(function(){
   $(".coin6").click(function(){
     for (var i=colum_6[0] ; i>colum_6.length; i-=8){
 
-      if (gridval[i].innerText == ""){
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
@@ -201,7 +226,7 @@ $(document).ready(function(){
   $(".coin7").click(function(){
 
     for (var i=colum_7[0] ; i>colum_7.length; i-=8){
-      if (gridval[i].innerText == ""){
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
@@ -221,7 +246,7 @@ $(document).ready(function(){
   $(".coin8").click(function(){
     for (var i=colum_8[0] ; i>colum_8.length; i-=8){
 
-      if (gridval[i].innerText == ""){
+      if (gridval[i].innerText == "a" || gridval[i].innerText == "b"){
         if (player_1 == 1) {
           // gridval[i].css = player_1;
           // gridval[i].innerText= $("#col1").css("background-color","red");
@@ -235,126 +260,92 @@ $(document).ready(function(){
         }
       }
     }
-    calculateWinner();
-    alert('winner found')//somehow stop the game here for all buttons... if(calculateWinner) { then do shit }
+    calculateWinner(); //somehow stop the game here for all buttons... if(calculateWinner) { then do shit }
   });
 
-var calculateWinner = function() {
-  console.log('calcWInner Called');
-  // winnerInDiagonalRight();
-  // winnerInDiagonalLeft();
-  // // NOTE: test winnerInDiagonalLeft() and winnerInRow(). then complete
-  if(winnerInColumn() /*|| winnerInRow() */|| winnerInDiagonalRight() || winnerInDiagonalLeft()) {
-    console.log('WINNER FOUND 1');
-    return true;
+  var calculateWinner = function() {
+    // console.log('calcWInner Called');
+    /*testing winning parametres
+    // winnerInDiagonalRight();
+    // winnerInDiagonalLeft();
+    // winnerInColumn()
+    // winnerInRow();*/
+    // NOTE: test winnerInDiagonalLeft() and winnerInRow(). then complete
+    if(winnerInColumn() || winnerInRow() || winnerInDiagonalRight() || winnerInDiagonalLeft()) {
+      if (player_1 == 1) {
+        alert('WINNER FOUND P2');
+        player_1 = 0;
+      }else{
+        alert('WINNER FOUND P1');
+        player_1 = 1;
+      }
+
+    }
   }
-}
 
-var winnerInColumn = function() { //check for winners in columns
-  console.log('verticalWinnerChecks called');
-  for (var i = 1; i < 9; i++) {
-    console.log('@ i = ' + i);
-    var colArray = getColumnArray(i);
-    for( var j = 0; j < colArray.length-3; j++ ) {
-      if((gridval[colArray[j]].innerText== '1')&&
-      (gridval[colArray[j+1]].innerText== '1') &&
-      (gridval[colArray[j+2]].innerText== '1')&&
-      (gridval[colArray[j+3]].innerText== '1'))
+  var winnerInColumn = function() { //check for winners in columns
+    // console.log('verticalWinnerChecks called');
+    for (var i = 1; i < 9; i++) {
+      // console.log('@ i = ' + i);
+      var colArray = getColumnArray(i);
+      for (var j = 0; j < colArray.length -3; j++ ) {
+        if(gridval[colArray[j]].innerText == gridval[colArray[j+1]].innerText) {
+          if(gridval[colArray[j+1]].innerText == gridval[colArray[j+2]].innerText && gridval[colArray[j+2]].innerText == gridval[colArray[j+3]].innerText) {
+            // console.log('FOUND WINNER!: at i  = ' + i + ' and j = ' + j);
+            // alert("found winner in winnerIncolumn")
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  };
 
-
-      // if(gridval[colArray[j]].innerText == gridval[colArray[j+1]].innerText) {
-      //   if(gridval[colArray[j+1]].innerText == gridval[colArray[j+2]].innerText && gridval[colArray[j+2]].innerText  == gridval[colArray[j+3]].innerText)
-         {
-           alert("winner wineer chicken dinner")
-           console.log("can see 1");
-          // console.log('FOUND WINNER!: at i  = ' + i + ' and j = ' + j);
+  var winnerInRow = function() {
+    // console.log('invoking winnerInRow');
+    for (var r = 0; r < 8; r++){
+      var rowArray = getRowArray(r);
+      //checking that rows are returned not columns
+      // console.log(rowArray);
+      for (var rw = 0; rw < rowArray.length -3; rw++){
+        if(gridval[rowArray[rw]].innerText == gridval[rowArray[rw+1]].innerText){
+          if(gridval[rowArray[rw+1]].innerText == gridval[rowArray[rw+2]].innerText && gridval[rowArray[rw+2]].innerText == gridval[rowArray[rw+3]].innerText){
+            // console.log('Winner found in winnerInRow');
+            // alert("found winner in winnerInRow")
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+  var winnerInDiagonalRight = function() {
+    // console.log('invoking winnerInDiagonalRight...');
+    for (var i = 0; i < validStartingSquaresDRWin().length; i++) {
+      var currentReferenceSquare = validStartingSquaresDRWin()[i];
+      if(gridval[currentReferenceSquare].innerText == gridval[currentReferenceSquare - 7].innerText
+        && gridval[currentReferenceSquare - 7].innerText == gridval[currentReferenceSquare - 14].innerText
+        && gridval[currentReferenceSquare - 14].innerText == gridval[currentReferenceSquare - 21].innerText ) {
+          // console.log('Winner found diagonal right at currentReferenceSquare = ' + currentReferenceSquare );
+          // alert("found winnerInDiagonalRight")
           return true;
         }
       }
+      return false;
+    };
 
-  }
-  return false;
-};
-
-var winnerInRow = function() {
-  //reference winner in column
-}
-
-var winnerInDiagonalRight = function() {
-  console.log('invoking winnerInDiagonalRight...');
-  for (var i = 0; i < validStartingSquaresDRWin().length; i++) {
-    var currentReferenceSquare = validStartingSquaresDRWin()[i];
-    if(gridval[currentReferenceSquare].innerText == gridval[currentReferenceSquare - 7].innerText
-        && gridval[currentReferenceSquare - 7].innerText == gridval[currentReferenceSquare - 14].innerText
-         && gridval[currentReferenceSquare - 14].innerText == gridval[currentReferenceSquare - 21].innerText ) {
-            console.log('Winner found diagonal right at currentReferenceSquare = ' + currentReferenceSquare );
+    var winnerInDiagonalLeft = function() {
+      //reference diagonal right
+      // console.log('invoking winnerInDiagonalLeft...');
+      for (var i= 0; i < validStartingSquaresDlWin().length; i++){
+        var dLcurrentRefSquare = validStartingSquaresDlWin()[i];
+        if (gridval[dLcurrentRefSquare].innerText == gridval[dLcurrentRefSquare -9].innerText
+          && gridval[dLcurrentRefSquare -9].innerText == gridval[dLcurrentRefSquare -18].innerText && gridval[dLcurrentRefSquare -18].innerText == gridval[dLcurrentRefSquare -27].innerText) {
+            // console.log('winner found diagnol left at dlcurrentRefSquare = ' + dLcurrentRefSquare);
+            // alert("found winner in winnerInDiagonalRight")
             return true;
           }
-  }
-  return false;
-};
-
-var winnerInDiagonalLeft = function() {
-  //reference diagonal right
-  console.log('invoking winnerInDiagonalLeft...');
-  for (var i= 0; i < validStartingSquaresDlWin().length; i++) {
-    var dLcurrentRefSquare = validStartingSquaresDlWin()[i];
-    if(gridval[dLcurrentRefSquare].innerText == gridval[dLcurrentRefSquare -9].innerText
-    && gridval[dLcurrentRefSquare -9].innerText == gridval[dLcurrentRefSquare -18].innerText
-    && gridval[dLcurrentRefSquare -18].innerText ==  gridval[dLcurrentRefSquare -27].innerText) {
-      console.log('winner found diagnol left at dlcurrentRefSquare = ' + dlcurrentRefSquare);
-      return true;
-    }
-
-  }
-  return false;
-};
-
-
-// var array = gridval;
-// var row = array.length;
-// var cols = array[8].length;
-//   for (i=1; i<=2; i++){/*left to right*/
-// for (cols = 0; cols <=6; cols++){
-//   for (row = 0; row <=6; row++){
-//     if(gridval[row][cols] == i){
-//       if ((gridval[row][cols+1] == i) &&
-//       (gridval[row][cols+2] == i) &&
-//       (gridval[row][cols+3 == i])) {
-//         endGame(i);
-//         return true;
-//        }
-//       }
-//     }
-//   }
-// }
-
-//   }
-//
-//  }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+        }
+        return false;
+      };
+    })
